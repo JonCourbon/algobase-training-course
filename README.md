4a2e66d4e7ac15440c9e29f5ec64b39a81a58ed3
Jonathan Courbon <jonathan.courbon@uca.fr>
2019-07-30 14:22:12 +0200

    Correction ReadMe.md

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Exercices de découverte de l'algorithmie
 DUT MMI - 1ère année - Adaptation de parcours
 IUT Le Puy-en-Velay
+
+Programmation en [TypeScript](https://www.typescriptlang.org/ "https://www.typescriptlang.org/")
+
+Utilisation basique:
+1. modifier le contenu du fichier moncode.ts (voir fichiers d'exemple)
+2. Compilation: tsc moncode.ts
+
+Ouvrir la page HTML index.html dans un navigateur pour voir le résultat !
+
--- moncode.ts
+++ moncode.ts
@@ -1,23 +1,3 @@
 /// <reference path="fonctions.ts"/>
 
-// Declaration des événements
-document.getElementById('effacer')
-		.addEventListener("click", boutonEffacerAppuye);
-		
-document.getElementById('envoyer')
-		.addEventListener("click", boutonEnvoyerAppuye);
-
-function boutonEffacerAppuye() : void{
-	effacerDessin();
-}
-
-function boutonEnvoyerAppuye() : void{
-	// on lit la valeur du texte
-	var message=recupererTexteZone("zonetexte");
-	afficherTexte(message);
-}
-
 creerZoneDessin();
-dessinerRectangle(10,10,200,300);
-
-
