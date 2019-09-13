81f2ba6e673eeb670dbbc7bd17816fa449bab106
Jonathan Courbon <jonathan.courbon@uca.fr>
2019-09-13 22:57:54 +0200

    Reset moncode

--- moncode.js
+++ moncode.js
@@ -1,25 +1,2 @@
 /// <reference path="fonctions.ts"/>
 creerZoneDessin();
-var nbrectangles_max = largeurZone() / 100;
-var nbrectangles = lireNombre("Combien de rectangles voulez-vous afficher ?");
-var couleur;
-if (nbrectangles > nbrectangles_max) {
-    console.log("Valeur trop grande, nous ne tracerons que " + nbrectangles_max + " rectangles");
-    nbrectangles = nbrectangles_max;
-}
-var norectangle = 0;
-for (norectangle = 0; norectangle < nbrectangles; norectangle++) {
-    if (norectangle == 1) {
-        couleur = "pink";
-    }
-    else if (norectangle == 2) {
-        couleur = "green";
-    }
-    else if (norectangle == 3) {
-        couleur = "red";
-    }
-    else {
-        couleur = "blue";
-    }
-    dessinerRectangleRempli(100 * norectangle, 200, 10, 100, couleur);
-}
--- moncode.ts
+++ moncode.ts
@@ -1,29 +1,2 @@
 /// <reference path="fonctions.ts"/>
 creerZoneDessin();
-let nbrectangles_max:number=largeurZone()/100;
-let nbrectangles:number=lireNombre("Combien de rectangles voulez-vous afficher ?");
-
-let couleur:string;
-
-if(nbrectangles>nbrectangles_max)
-{
-	console.log("Valeur trop grande, nous ne tracerons que "+nbrectangles_max+ " rectangles");
-	nbrectangles=nbrectangles_max;
-}
-	
-let norectangle:number=0;
-for(norectangle=0;norectangle<nbrectangles;norectangle++){
-	if(norectangle == 0) {
-		couleur="pink";
-	}
-	else if(norectangle == 1) {
-		couleur="green";
-	}
-	else if(norectangle == 2) {
-		couleur="red";
-	}
-	else {
-		couleur="blue";
-	}
-	dessinerRectangleRempli(100*norectangle,200,10,100,couleur);
-}
